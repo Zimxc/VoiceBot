fedee1e464eec0e006fd973322be63f8a2757570
Zimxc <15firekot@gmail.com>
2024-07-14 13:25:01 +0300

    Update voiceBot.py

--- voiceBot/voiceBot.py
+++ voiceBot/voiceBot.py
@@ -1,31 +1,41 @@
 import speech_recognition as sr
-
+import webbrowser
+import subprocess
 recognizer = sr.Recognizer()
-
-
 def capture_voice_input():
-    with sr.Microphone() as sourse:
-        print('Listening...')
-        audio = recognizer.listen(sourse)    
-        
+    with sr.Microphone() as source:
+        print("Слухаю...")
+        audio = recognizer.listen(source)
     return audio
-
-
 def convert_voice_to_text(audio):
     try:
-        text =recognizer.recognize_google(audio)
-        print('You said:  '+ text)
+        text = recognizer.recognize_google(audio, language = 'uk-UK')
+        print("Ви сказали:" + text)
     except sr.UnknownValueError:
-        text = ''
-        print('-_-')
+        text = ""
+        print("Вибачте, я не зрозумів, повторіть ще раз")
     except sr.RequestError as e:
         text = ""
         print("Error; {0}".format(e))
     return text
-
+def process_voice_command(text):
+    if "привіт" in text.lower():
+        print("Привіт! як я можу вам допомогти?")
+    elif "як справи" in text.lower():
+        print('погано')
+    elif "прощавай" in text.lower():
+        print("До побачення!Гарного дня!")
+    elif "калькулятор" in text.lower():
+        subprocess.call(['calc'])
+        return True
+    else:
+        print("Я Вас не розумію. Повторіть Ваш запит")
+    return False
 def main():
-    audio = capture_voice_input()
-    text = convert_voice_to_text(audio)
-
-if __name__ == '__main__':
+    end_program = False
+    while not end_program:
+        audio = capture_voice_input()
+        text = convert_voice_to_text(audio)
+        end_program = process_voice_command(text)
+if __name__ == "__main__":
     main()
